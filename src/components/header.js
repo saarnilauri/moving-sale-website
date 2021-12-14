e2545763ed3195ebf128232255441740bab9de31
Lauri Saarni <lauri.saarni@gmail.com>
2021-12-14 15:29:36 +0700

    Remove link from header logo

--- src/components/header.js
+++ src/components/header.js
@@ -8,9 +8,7 @@ const Header = ({ onHideNav, onShowNav, showNav, siteTitle, logo }) => (
   <div className="relative z-50 border-b-8 border-gray-100">
     <div className="container mx-auto flex p-5">
       <div className="flex-1">
-        <Link to="/">
-          <SanityGatsbyImage node={logo} alt={siteTitle} />
-        </Link>
+        <SanityGatsbyImage node={logo} alt={siteTitle} />
       </div>
 
       <button
